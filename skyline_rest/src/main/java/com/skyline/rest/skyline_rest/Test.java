47bebb9622ee66a998df56ae93f70647cf77f9f3
epoxy <palmqvist.anton@gmail.com>
2013-10-04 16:43:46 +0200

    added src package

---
+++ skyline_rest/src/main/java/com/skyline/rest/skyline_rest/Test.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.skyline.rest.skyline_rest;
+
+/**
+ *
+ * @author Epoxy
+ */
+public class Test {
+    
+}
