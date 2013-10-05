f7f0aa2f66e481d17aab8227f049fec7d908eb7b
Tomas <tomsel1@hotmail.com>
2013-10-05 13:14:25 +0200

    made it possible to get data from model to skyline_rest bug fix

---
+++ skyline_rest/src/main/java/com/skyline/rest/skyline_rest/Blog.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.skyline.rest.skyline_rest;
+
+import com.skyline.model.core.BlogFactory;
+import com.skyline.model.core.IBlog;
+import com.skyline.model.core.IMembersRegistry;
+import com.skyline.model.core.IPostRegistry;
+
+/**
+ *
+ * @author hajo modified by tomassellden
+ */
+public enum Blog {
+
+    INSTANCE;
+    private final IBlog blog;
+
+    private Blog() {
+        blog = BlogFactory.getBlog(true);
+    }
+
+    public IMembersRegistry getMembersRegistry() {
+        return blog.getMemberContainer();
+    }
+
+    public IPostRegistry getPostContainer() {
+        return blog.getPostContainer();
+    }
+}
