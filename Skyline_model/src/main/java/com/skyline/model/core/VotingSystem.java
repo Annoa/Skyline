2e86eef73ffc538840ad9b851bbdd430adef6796
Krabban91 <anderssongabriel665@gmail.com>
2013-10-09 16:35:40 +0200

    Added addAllOnPost(), corrected a type-O in VS.java

--- Skyline_model/src/main/java/com/skyline/model/core/VotingSystem.java
+++ Skyline_model/src/main/java/com/skyline/model/core/VotingSystem.java
@@ -9,60 +9,61 @@ package com.skyline.model.core;
  * @author Gabriel
  */
 public class VotingSystem implements Comparable<Object> {
-    
+
     private int upVote;
     private int downVote;
-    
-    public VotingSystem(){
-        this(0,0);
+
+    public VotingSystem() {
+        this(0, 0);
     }
-    
-    public VotingSystem(int up, int down){
+
+    public VotingSystem(int up, int down) {
         this.upVote = up;
         this.downVote = down;
     }
-    
-    public int getUpVote(){
+
+    public int getUpVote() {
         return upVote;
     }
-    
-    public int getDownVote(){
+
+    public int getDownVote() {
         return downVote;
     }
-    
-    public int getValue(){
-        return upVote-downVote;
+
+    public int getValue() {
+        return upVote - downVote;
     }
-    
+
     @Override
-    public boolean equals(Object o){
-        if (o instanceof VotingSystem){
-            VotingSystem vS = (VotingSystem)o;
+    public boolean equals(Object o) {
+        if (o instanceof VotingSystem) {
+            VotingSystem vS = (VotingSystem) o;
             return this.getValue() == vS.getValue();
         }
         return false;
     }
-    
+
     @Override
-    public String toString(){
-        return "Votes = \n{ upVotes = " + upVote 
-                + " \ndownVotes = " + downVote 
-                + "\nvalue = " + (upVote-downVote);
+    public String toString() {
+        return "Votes = \n{ upVotes = " + upVote
+                + " \ndownVotes = " + downVote
+                + "\nvalue = " + getValue();
     }
 
     /**
-     * 
+     *
      * @return 1 if self is greater
      */
     public int compareTo(Object o) {
-        if (o instanceof VotingSystem){
-            if(this.equals(o))
+        if (o instanceof VotingSystem) {
+            if (this.equals(o)) {
                 return 0;
-            VotingSystem vs = (VotingSystem)o;
-            if(this.getValue()>vs.getValue())
-            return 1;
+            }
+            VotingSystem vs = (VotingSystem) o;
+            if (this.getValue() > vs.getValue()) {
+                return 1;
+            }
         }
         return -1;
     }
-     
 }
--- skyline_rest/src/main/java/com/skyline/rest/skyline_rest/CommentResource.java
+++ skyline_rest/src/main/java/com/skyline/rest/skyline_rest/CommentResource.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.skyline.rest.skyline_rest;
 
 import com.skyline.model.core.Comment;
@@ -9,8 +5,6 @@ import com.skyline.model.core.Member;
 import com.skyline.model.core.Post;
 import com.skyline.model.core.VotingSystem;
 import com.skyline.model.utils.IDAO;
-import com.sun.jndi.toolkit.url.Uri;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -20,14 +14,14 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -40,22 +34,27 @@ public class CommentResource {
     private IDAO<Comment, Long> comments = Blog.INSTANCE.getCommentContainer();
     private IDAO<Member, Long> members = Blog.INSTANCE.getMembersRegistry();
     private IDAO<Post, Long> posts = Blog.INSTANCE.getPostContainer();
-    @Context
-    private UriInfo uriInfo;
 
+    //TODO: Is it FormParam or PathParam?
     @GET
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getAll() {
+    public Response getAllOnPost(@FormParam("postId") Long postId) {
         List<Comment> commentList = comments.getRange(0, comments.getCount());
         List<CommentProxy> wrappedComments = new ArrayList();
-        for (Comment c : commentList) {
-            wrappedComments.add(new CommentProxy(c));
+        try {
+            for (Comment c : commentList) {
+                if (c.getPost().equals(posts.find(postId))) {
+                    wrappedComments.add(new CommentProxy(c));
+                }
+            }
+            GenericEntity<List<CommentProxy>> ge =
+                    new GenericEntity<List<CommentProxy>>(wrappedComments) {
+            };
+            return Response.ok(ge).build();
+        } catch (IllegalArgumentException ie) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
-        GenericEntity<List<CommentProxy>> ge =
-                new GenericEntity<List<CommentProxy>>(wrappedComments) {
-        };
-
-        return Response.ok(ge).build();
     }
 
     @GET
@@ -103,48 +102,69 @@ public class CommentResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response update(@PathParam("Id") Long id,
-            @FormParam("postId") Long postId,
-            @FormParam("parentId") Long parentId,
-            @FormParam("memberId") Long memberId,
-            @FormParam("date") Long date,
-            @FormParam("upVote") int upVote,
-            @FormParam("downVote") int downVote,
-            @FormParam("text") String text) {
-        Date d = new Date(date);
-        Member postAuthor = members.find(memberId);
-        Post p = posts.find(postId);
-        Comment cp = comments.find(parentId);
+                @FormParam("postId") Long postId,
+                @FormParam("parentId") Long parentId,
+                @FormParam("memberId") Long memberId,
+                @FormParam("date") Long date,
+                @FormParam("upVote") int upVote,
+                @FormParam("downVote") int downVote,
+                @FormParam("text") String text) {
         try {
-            comments.update(new Comment(id, p, cp, text, d, postAuthor, new VotingSystem(upVote, downVote)));
+            Member postAuthor = members.find(memberId);
+            Post p = posts.find(postId);
+            Comment cp = comments.find(parentId);
+            Date d = new Date(date);
+            comments.update(new Comment(id, p, cp, text, d, postAuthor, 
+                    new VotingSystem(upVote, downVote)));
             return Response.ok().build();
         } catch (IllegalArgumentException e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
-    /*
-     @GET
-     @Path("range")
-     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-     public Response getRange(@QueryParam("first") int first,
-     @QueryParam("last") int last) {
-     List<Post> tmpPosts = postBox.getRange(first, last);
-     List<PostProxy> postList = new ArrayList<PostProxy>();
-     for (Post post : tmpPosts) {
-     postList.add(new PostProxy(post));
-     }
-     GenericEntity<List<PostProxy>> ge = new GenericEntity<List<PostProxy>>(postList) {
-     };
-     return Response.ok(ge).build();
-     }
 
-     @GET
-     @Path("count")
-     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-     public Response getCount() {
-     Integer i = new Integer(postBox.getCount());
-     PrimitiveJSONWrapper<Integer> pj= new PrimitiveJSONWrapper<Integer>(i);
-     return Response.ok(pj).build();
-     }
+    @GET
+    @Path("range")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getRange(@QueryParam("first") int first,
+            @QueryParam("last") int last) {
+        List<Comment> tmpC = comments.getRange(first, last);
+        List<CommentProxy> cP = new ArrayList<CommentProxy>();
+        for (Comment c : tmpC) {
+            cP.add(new CommentProxy(c));
+        }
+        GenericEntity<List<CommentProxy>> ge =
+                new GenericEntity<List<CommentProxy>>(cP) {
+        };
+        return Response.ok(ge).build();
+    }
+
+    @GET
+    @Path("count")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getCount() {
+        int i = comments.getCount();
+        PrimitiveJSONWrapper<Integer> pj = new PrimitiveJSONWrapper<Integer>(i);
+        return Response.ok(pj).build();
+    }
+
+    /**
+     * @deprecated no need to get all comments. Use getAllOnPost(postId)
+     * instead.
+     * @return all comments in Skyline
      */
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getAll() {
+        List<Comment> commentList = comments.getRange(0, comments.getCount());
+        List<CommentProxy> wrappedComments = new ArrayList();
+        for (Comment c : commentList) {
+            wrappedComments.add(new CommentProxy(c));
+        }
+        GenericEntity<List<CommentProxy>> ge =
+                new GenericEntity<List<CommentProxy>>(wrappedComments) {
+        };
+
+        return Response.ok(ge).build();
+    }
 }
