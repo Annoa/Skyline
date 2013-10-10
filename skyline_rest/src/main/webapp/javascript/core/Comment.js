8974b23f6fce86ff8e1e92f50c29ba0b4c2981a3
Krabban91 <anderssongabriel665@gmail.com>
2013-10-10 08:49:06 +0200

    started working on javascript for comments

---
+++ skyline_rest/src/main/webapp/javascript/core/Comment.js
@@ -0,0 +1,49 @@
+
+var PostBox = function(baseUri) {
+    this.baseUri = baseUri;
+};
+
+
+
+PostBox.prototype = (function() {
+
+    return {
+        getAllOnPost: function() {
+            return $.getJSON(this.baseUri);
+        },
+        add: function(comment) {
+            return $.ajax({
+                type: 'POST',
+                url: this.baseUri,
+                data: comment
+            });
+        },
+        remove: function(id) {
+            return $.ajax({
+                type: 'DELETE',
+                url: this.baseUri + "/" + id
+            });
+        },
+        find: function(id) {
+            return $.getJSON(this.baseUri + "/" + id);
+        },
+        getRange: function(fst, max) {
+            //TODO
+            return $.getJSON(this.baseUri + "/range?fst=" + fst + "&max=" + max);
+        },
+        getCount: function() {
+            return $.getJSON(this.baseUri + "/count");
+        },
+        update: function(post) {
+            return $.ajax({
+                type: 'PUT',
+                url: this.baseUri + "/" + post.id,
+                data: post
+            });
+        }
+        //TODO
+//        getPostByMember: function ( post ) {
+//            
+//        }
+    };
+}());
