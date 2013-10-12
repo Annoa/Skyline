25686b2bca36121f62f6ee469bf3e6bb0f7f74ca
epoxy <palmqvist.anton@gmail.com>
2013-10-12 21:47:24 +0200

    added js for rendering all posts as <div>s

--- skyline_rest/src/main/webapp/javascript/gui/wall.js
+++ skyline_rest/src/main/webapp/javascript/gui/wall.js
@@ -4,25 +4,38 @@
  */
 
 $(function() {
-    //    console.log("walls javascript körs");
-    //    var postContainer = allPosts.getRange(0, 2).
-    ////    console.log("walls javascript körs222222" + allPosts);
-    //    console.log(allPosts);
-    //    console.log(postContainer);
-    //    
-    skyline.getPostBox().getAll().done(function(post){
+    //Clear the table
+    //    $('#posts tbody').remove();
+    console.log("walls javascript körs");
+    
+    skyline.getPostBox().getAll().done(renderTable);
+    
+    /**********************************************
+     *   
+     *   Function for redering table of all wall posts
+     */
+    function renderTable(post) {
         console.log(post[0]);
+        //        for(var i=0; i<post.length; i++){
+        //            console.log(post[i]);
+        //            $('#posts').append("<tr><td>" + post[i].id + post[i].title + "</td></tr>");
+        //            //                    + "<td>" + post[i].name + "</td>"
+        //            //                    + "<td>" + post[i].price + "</td></tr>");
+        //        }
+        var htmlText = '';
         for(var i=0; i<post.length; i++){
+            //            htmlText += '<div id="div'+ i +'" />';
+            htmlText += '<div>'
+                    + 'Title: ' + post[i].title + '<br>' 
+                    + 'Date: ' + post[i].date + '<br>' 
+                    + 'Up Votes = ' + post[i].upVotes + '<br>'
+                    + 'Down Votes = ' + post[i].downVotes + '<br>'
+                    + '<br>'
+                    + 'Best regards<br>User Nr ' + post[i].id + '<br>' 
+                    + '<br><br>\n\
+                        </div>';
             console.log(post[i]);
-            $('#posts').append("<tr><td>" + post[i].id + "</td></tr>");
-//                    + "<td>" + post[i].name + "</td>"
-//                    + "<td>" + post[i].price + "</td></tr>");
         }
-    });
-    
-    
-    $('#posts tbody').remove();
-    
-    //Create the table of posts
-    
+        $('body').append(htmlText);
+    }
 });
