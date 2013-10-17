625911be8184ba3850cbb58f7a1309ec6518c643
Anno <anno.tao@gmail.com>
2013-10-17 17:38:21 +0200

    Added bootstrap and made some functionality changes

---
+++ skyline_rest/src/main/webapp/javascript/gui/home.js
@@ -0,0 +1,48 @@
+/**
+ * Js for index page, the tabs
+ */
+$(document).ready(function() {
+    
+    $("#contents").load("content/wall.html");
+    
+    $("#tabs").tabs({
+        beforeLoad: function(event, ui) {
+            ui.jqXHR.error(function() {
+                ui.panel.html(
+                        "Couldn't load this tab. We'll try to fix this as soon as possible. " +
+                        "If this wouldn't be a demo.");
+            });
+        }
+    });
+    
+    $("#wall").click(function(event) {
+        $("#contents").load("content/wall.html");
+        resetActive();
+        $("#wall").parent().addClass("active");
+        
+        event.preventDefault();
+    });
+    
+    $("#myPage").click(function(event) {
+        $("#contents").load("content/myPage.html");
+        resetActive();
+        $("#myPage").parent().addClass("active");
+        
+        event.preventDefault();
+    });
+    
+    $("#members").click(function(event) {
+        $("#contents").load("content/members.html");
+        resetActive();
+        $("#members").parent().addClass("active");
+        
+        event.preventDefault();
+    });
+    
+    function resetActive() {
+        $(".menuItem").parent().removeClass("active");
+    };
+});
+
+
+
