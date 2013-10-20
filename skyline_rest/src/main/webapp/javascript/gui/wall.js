3e804f0b3ea15a5a717c1fcda694631cfeeed3c6
Anno <anno.tao@gmail.com>
2013-10-20 16:30:11 +0200

    Added last of the comment functionality.

--- skyline_rest/src/main/webapp/javascript/gui/wall.js
+++ skyline_rest/src/main/webapp/javascript/gui/wall.js
@@ -10,7 +10,6 @@ $(function() {
     $("#aboutUrl2").remove();
     //Clear the table
     //    $('#posts tbody').remove();
-    console.log("walls javascript körs");
     skyline.getPostBox().getAll().done(renderAllPosts);
     
     //Eventhandling when clicking on a post
@@ -25,7 +24,6 @@ $(function() {
     //Button: Save post form
     $("#save-post").button().click(function(){
         var newPost = getFormDialogData();
-        console.log(newPost);
         clearFormDialogData();
         $("#new-post").attr("hidden",'hidden');
         skyline.getPostBox().add(newPost).then(renderAddedPost(newPost));
@@ -62,8 +60,10 @@ $(function() {
      * @returns {undefined}
      */
     function renderComments(post) {
+
         skyline_comments.getCommentBox().getRootCommentsForPost(post).done(commentDraw);
         
+       
         function commentDraw(comments) {
             
             if ($('[id="comments-post#' + post + '"]').is(':empty')) {
@@ -74,9 +74,13 @@ $(function() {
                 });
                 function recurseChildren(i, parent) {
                     var color = ((even) ? 'even' : 'uneven');
-                    htm += '<div class="comment comment-' + color + '"><div>\n\
+                    htm += '<div class="comment comment-' + color + ' comment_'+ parent.id +'"><div>\n\
                     <span class="glyphicon glyphicon-arrow-up"></span>\n\
-                    <span class="glyphicon glyphicon-arrow-down"></span></div><div><p>'+ parent.commentText + '</p></div>';
+                    <span class="glyphicon glyphicon-arrow-down"></span></div><div><p>'+ parent.commentText + '</p></div>'
+                    + '<div class="btn-group-xs">'
+                    + '<button class="btn btn-default comment-reply-button" type="button">Reply</button>'
+                    + '</div>'
+                    + '<div class="add-commentbox-post_' + post + '-comment_' + parent.id + '"></div>';
                     if (undefined !== parent.childComments){
                         even = !even;
                         $.each(parent.childComments, function(z, child) {
@@ -87,6 +91,72 @@ $(function() {
                     htm +='</div>';
                 };
                 $('[id="comments-post#' + post + '"]').append(htm);
+                
+                $('[id="comments-post#' + post + '"]')
+                        .find(".comment-reply-button")
+                        .button()
+                        .click(function() {
+                    var targetClasses = $(this).parent().parent().attr('class');
+                    var targetId = targetClasses.substr(targetClasses.indexOf("_")+1);
+                    
+                    var targetDiv = $(this).parent().parent().find("[class^=add-commentbox-post_]");
+                    if ($(targetDiv).is(":empty")) {
+                        var htm = addCommentBoxWithParent(targetId);
+                        $(targetDiv).append(htm);
+                        
+                        $(targetDiv).find(".comment-save-button")
+                                .button()
+                                .click(function() {
+                            var comment = {};
+                            
+                            comment.text = $(targetDiv).find(".comment-textarea").val();
+                            comment.postId = post;
+                            comment.parentId = targetId;
+                            //******
+                            // Test AUTHOR!
+                            comment.authorId = 1;
+                            //******
+                            
+                            $(targetDiv).find(".comment-textarea").val("");
+                            skyline_comments.getCommentBox().add(comment).done(function() {
+                                console.log("Trying to render comments for post: " + post);
+                                location.reload();
+                            });
+                        });
+                        $(targetDiv).find(".comment-cancel-button")
+                                .button()
+                                .click(function() {
+                            $(targetDiv).hide();
+                        });
+                        
+                    } else {
+                        if ($(targetDiv).is(':hidden')) {
+                            $(targetDiv).show();
+                        } else {
+                            $(targetDiv).hide();
+                        }
+                    }
+                });
+                        
+//                        $(this)
+//                        .button()
+//                        .click(function() {
+//                    console.log($(this).parent());
+//                    })
+//                   );     
+                
+                function addCommentBoxWithParent(parentComment) {
+                    var commentHtml = '<form id="add-commentbox-post_'+ post +'-parent_' + parentComment + '" class="write-post-form" >'
+                        + '<div class="input-group">'
+                            +'<textarea class="form-control input-block-level comment-textarea" placeholder="Enter comment text here..."/>'
+                        +'</div>'
+                        +'<div class="btn-group-xs">'
+                            +'<button class="btn btn-default btn-success comment-save-button" type="button" >Save</button>'
+                            +'<button class="btn btn-default btn-danger comment-cancel-button" type="button" >Cancel</button>'
+                        +'</div>'
+                    +'</form>';
+                    return commentHtml;
+                };
             }
             return false;
         }
@@ -99,7 +169,6 @@ $(function() {
      * @returns {undefined}
      */
     function renderAllPosts(post) {
-        console.log(post[0]);
         $("#postlist").contents().remove();
         var htmlText = '';
         for(var i=0; i<post.length; i++){
@@ -119,6 +188,7 @@ $(function() {
                     // If no comments where gotten we don't change name
                     if (!$(targetDiv).is(':empty')) {
                         $(this).html("Hide comments");
+                        
                     }
                 } else {
                     $(this).html("Show comments");
@@ -145,15 +215,16 @@ $(function() {
                             
                             comment.text = $(tar).find(".comment-textarea").val();
                             comment.postId = target;
+                            comment.parentId = -1;
                             //******
                             // Test AUTHOR!
                             comment.authorId = 1;
                             //******
-                            console.log(comment);
                             $(tar).find(".comment-textarea").val("");
-                            $("#new-post").attr("hidden",'hidden');
-                            skyline_comments.getCommentBox().add(comment)
-                                    .then($("#contents").load("content/wall.html"));
+                            skyline_comments.getCommentBox().add(comment).done(function() {
+                                console.log("Trying to render comments for post: " + target);
+                                location.reload();
+                            });
                         });
                     $(tar).find(".comment-cancel-button")
                                 .button()
@@ -176,8 +247,8 @@ $(function() {
                     +'<textarea class="form-control input-block-level comment-textarea" placeholder="Enter comment text here..."/>'
                 +'</div>'
                 +'<div class="btn-group-xs">'
-                    +'<button class="btn btn-default comment-save-button" type="button" >Save</button>'
-                    +'<button class="btn btn-default comment-cancel-button" type="button" >Cancel</button>'
+                    +'<button class="btn btn-default btn-success comment-save-button" type="button" >Save</button>'
+                    +'<button class="btn btn-default btn-danger comment-cancel-button" type="button" >Cancel</button>'
                 +'</div>'
             +'</form>';
             return html;
@@ -195,8 +266,6 @@ $(function() {
      */
     function renderAddedPost(post) {
         //        $('#postlist').append(htmlText);
-        console.log("LOL");
-        console.log(post);
         $('#postlist').append(convertPostToHTML(post));
     }
     /**
@@ -209,20 +278,22 @@ $(function() {
         var d = new Date(post.date);
         var video = convertToYouTubeEmbedLink(post.postVideo);
         var videoLink = (video.length === 0) ? '' : '<p><iframe width="420" height="345" src="' + video + '">' +' </iframe>' + '</p>';
-        return '<li id="post#' + post.id + '">'
+        return '<li id="post#' + post.id + '" class="post">'
                 + '<h4 class="custom-title">' + post.title + '</h2>' 
                 + '<p class="custom-date">Posted on: ' + d.getFullYear() + '-' + (d.getMonth()+1) + 
                     '-' + d.getDate() + '   ' + (d.getHours() < 10 ? '0' : '') + d.getHours() + 
                     ':' + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + '</p>' 
                 + '<p class="custom-body-text">' + post.bodyText + '</p>' 
                 + videoLink
-                + '<p>Up Votes = ' + post.upVotes + '</p>'
-                + '<p>Down Votes = ' + post.downVotes + '</p>'
                 + '<br>'
                 + '<p>Post ID: ' + post.id + '</p>'
-                + '<div class="btn-group btn-group-sm">'
+                + '<div class="btn-group btn-group-sm voting-div">'
                 + '<button id="comment-button#'+ post.id +'" class="btn btn-default">Show comments</button>'
                 + '<button id="comment-add-button#'+ post.id +'" class="btn btn-default">Add comments</button>'
+                + '<span class="voting-span">'
+                + '<span class="glyphicon glyphicon-arrow-up orangered">' + post.upVotes + '</span>'
+                + '<span class="glyphicon glyphicon-arrow-down arrow-span periwinkle">' + post.downVotes + '</span>'
+                + '</span>'
                 + '</div>'
                 + '<div id="add-commentbox-post_'+post.id+'" class="commentbox"></div>'
                 + '<div id="comments-post#'+post.id+'" class="commentbox"></div>'
@@ -236,15 +307,15 @@ $(function() {
      * @returns {String}
      */
     function convertToYouTubeEmbedLink (link) {
-        console.log("convertToEmbed");
+//        console.log("convertToEmbed");
         var videoSuffix = link.substring(link.length - 11, link.length)
-            console.log(link.search("youtube.com/watch?v=") + " <- Bad link if -1");
+//            console.log(link.search("youtube.com/watch?v=") + " <- Bad link if -1");
         if(link.search("youtube.com/watch")!==-1){
-            console.log("Good link");
+//            console.log("Good link");
             return "http://www.youtube.com/embed/" + videoSuffix;
         }
         else{
-            console.log("Bad link, retype as: www.youtube.com/watch?v=.....");
+//            console.log("Bad link, retype as: www.youtube.com/watch?v=.....");
             return "";
             
         }
