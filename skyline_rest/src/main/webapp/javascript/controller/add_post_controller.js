4a71da5fb72233ee34bb83b12af2a3f4da353838
epoxy <palmqvist.anton@gmail.com>
2013-10-23 22:32:10 +0200

    removed unused code

--- skyline_rest/src/main/webapp/javascript/controller/add_post_controller.js
+++ skyline_rest/src/main/webapp/javascript/controller/add_post_controller.js
@@ -1,7 +1,3 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 $(document).ready(function() {
     $("#new-post").toggle();
     //Button add new post
